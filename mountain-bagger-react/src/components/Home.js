a05f9d3f7595d947c8f9ddbf0d10acc313005daa
Vitnija Bluzma <thepunkyone@gmail.com>
2019-07-05 01:17:27 +0100

    set viewport orientation, added gps watching

--- mountain-bagger-react/src/components/Home.js
+++ mountain-bagger-react/src/components/Home.js
@@ -12,6 +12,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SettingsIcon from '@material-ui/icons/Settings';
 import ExploreIcon from '@material-ui/icons/Explore';
 import GpsFixedIcon from '@material-ui/icons/GpsFixed';
+import GpsNotFixedIcon from '@material-ui/icons/GpsNotFixed';
 import SearchIcon from '@material-ui/icons/Search';
 import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 
@@ -43,6 +44,9 @@ class Home extends Component {
     super(props);
     this.state = {
       selectedTab: 'home',
+      lng: -3.2116,
+      lat: 54.4542,
+      locationWatchId: null,
     };
   }
 
@@ -52,16 +56,48 @@ class Home extends Component {
     this.setState({ selectedTab: selectedId });
   };
 
+  stopWatchingLocation = () => {
+    navigator.geolocation.clearWatch(this.state.locationWatchId);
+    this.setState({ lng: -3.2116, lat: 54.4542, locationWatchId: null });
+  };
+
+  watchUserLocation = () => {
+    const success = (position) => {
+      const { latitude, longitude, speed, altitude, heading } = position.coords;
+      this.setState({ lng: longitude, lat: latitude });
+    };
+
+    const error = () => {
+      alert('Unable to retrieve your location');
+    };
+
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 60000,
+      maximumAge: 60000,
+    };
+
+    if (!navigator.geolocation) {
+      console.log('Geolocation is not supported by your browser');
+    } else {
+      console.log('Locating…');
+      this.setState({
+        locationWatchId: navigator.geolocation.watchPosition(success, error, options),
+      });
+    }
+  };
+
   render() {
+    console.log(this.state);
     const { selectedTab } = this.state;
     return (
       <div className="Home">
         <nav className="UserNav nav-main">
-          <MenuIcon style={menuIconStyle} />
+          <MenuIcon style={{ ...menuIconStyle, cursor: 'pointer' }} />
           <h2>
             <Logo iconStyle={logoIconStyle} />
           </h2>
-          <SettingsIcon style={menuIconStyle} />
+          <SettingsIcon style={{ ...menuIconStyle, cursor: 'pointer' }} />
         </nav>
         <nav className="LocationNav nav-main">
           <div className="nav-metrics">
@@ -83,14 +119,28 @@ class Home extends Component {
             </p>
             <ExploreIcon style={menuIconLightStyle} />
           </div>
-          <GpsFixedIcon style={menuIconStyle} />
-          <SearchIcon style={menuIconStyle} />
+          {this.state.locationWatchId === null ?
+            (
+              <GpsNotFixedIcon
+                style={{ ...menuIconStyle, cursor: 'pointer' }}
+                onClick={() => this.watchUserLocation()}
+              />
+            )
+            :
+            (
+              <GpsFixedIcon
+                style={{ ...menuIconStyle, cursor: 'pointer' }}
+                onClick={() => this.stopWatchingLocation()}
+              />
+            )
+          }
+          <SearchIcon style={{ ...menuIconStyle, cursor: 'pointer' }} />
         </nav>
         <div className="content">
           <div>
             <Map />
             <span>
-              <CloudDownloadIcon style={downloadIconStyle} />
+              <CloudDownloadIcon style={{ ...downloadIconStyle, cursor: 'pointer' }} />
             </span>
           </div>
           {selectedTab === 'weather' && <Weather />}
